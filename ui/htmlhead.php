01cd822f1bd3c04da9beb2d122cd788d5b5d2f9e
heckerio <lawjunwei@gmail.com>
2021-05-31 01:04:48 +0800

    new updates

---
+++ ui/htmlhead.php
@@ -0,0 +1,16 @@
+<!-- htmlhead.php -->
+<!-- html head templates for hashtech's websites -->
+
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+</head>
+
+</html>
---
+++ ui/index.php
@@ -0,0 +1,22 @@
+<!-- index.html -->
+<!-- hashtech's main page -->
+
+<?php
+include "htmlhead.php";
+include "body.php";
+include "navbar.php";
+?>
+
+<!doctype html>
+<html lang="en">
+
+<head>
+    <title>hashtech</title>
+</head>
+
+<body>
+
+
+</body>
+
+</html>
